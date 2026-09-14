81ce704c01205ad8616c462d0a2d62e94587dfb1
ag03hans <ag03hans@siena.edu>
2026-09-14 10:54:12 -0400

    Added name and extra printout.

--- Hello210.java
+++ Hello210.java
@@ -2,12 +2,16 @@
 /**
  * CSIS 210 Setup Lab "Hello, World" program
  *
- * @author Prof. White
+ * @author Anna Hanson
  */
 public class Hello210 {
     
     public static void main(String args[]) {
         
         System.out.println("Hello CSIS 210 World!");
+        System.out.println("Hometown: Sonoma");
+        System.out.println("Favorite hometown resturant: Red Grape");
+        System.out.println("Favorite menu item: BBQ Pizza");
+        
     }
 }
